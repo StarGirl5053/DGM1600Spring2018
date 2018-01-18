658738da088fe8968a49173fb72743c28c7df960
Unknown <10612975@my.uvu.edu>
2018-01-18 11:25:57 -0700

    Created Animal Project
    Added print ("texts"); and OnMouseDown (Woofs);

---
+++ Second Game/Assets/AnimalScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("My animal is eating.");
+
+	
+		print ("Now my animal is sleeping.");
+
+		print ("I love my dogs.");
+		print ("Yes, I truly do.");
+	}
+
+	void OnMouseDown(){
+		print ("Woofs");
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
